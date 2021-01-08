9cec218f465d29a1ba3d6b9ce827278a2f41808a
Allex Radu <eusceptic@gmail.com>
2021-01-08 17:39:48 +0200

    fixing 'None' bugs

--- parse.py
+++ parse.py
@@ -23,7 +23,9 @@ product_tree = Et.parse(xml_file)
 
 root = product_tree.getroot()
 
-for each_product in root.findall('PRODUCT'):
+catalog = root.find('T_NEW_CATALOG')
+
+for each_product in catalog.findall('PRODUCT'):
     product_details = each_product.find('PRODUCT_DETAILS')
     product_order_details = each_product.find('PRODUCT_ORDER_DETAILS')
     product_price_details = each_product.find('PRODUCT_PRICE_DETAILS')
@@ -31,28 +33,44 @@ for each_product in root.findall('PRODUCT'):
     mime_info = each_product.find('MIME_INFO')
     user_defined_extensions = each_product.find('USER_DEFINED_EXTENSIONS')
     packing_units = user_defined_extensions.find('UDX.EDXF.PACKING_UNITS')
-    packing_unit = remove_none([unit if unit.find('UDX.EDXF.QUANTITY_MIN').text == '1' else None for unit in
-                                packing_units])
+    packing_unit = remove_none(
+        [unit if unit is not None and unit.find('UDX.EDXF.QUANTITY_MIN').text == '1' else None for unit in
+         packing_units]) if packing_units is not None else None
 
-    product_id = each_product.find('SUPPLIER_PID').text
-    international_pid = product_details.find('INTERNATIONAL_PID').text
-    manufacturer_pid = product_details.find('MANUFACTURER_PID').text
-    short_description = product_details.findall(".//DESCRIPTION_SHORT[@lang='eng']")[0].text
-    long_description = product_details.findall(".//DESCRIPTION_LONG[@lang='eng']")[0].text
-    manufacturer_name = product_details.find('MANUFACTURER_NAME').text
-    manufacturer_type_descr = product_details.find('MANUFACTURER_TYPE_DESCR').text
+    product_id = each_product.find('SUPPLIER_PID').text if each_product.find('SUPPLIER_PID') is not None else 'n/a'
+    print(product_id)
+    international_pid = product_details.find('INTERNATIONAL_PID').text if product_details.find(
+        'INTERNATIONAL_PID') is not None else 'n/a'
+    manufacturer_pid = product_details.find('MANUFACTURER_PID').text if product_details.find(
+        'MANUFACTURER_PID') is not None else 'n/a'
+    short_description = product_details.findall(".//DESCRIPTION_SHORT[@lang='eng']")[0].text if \
+        product_details.findall(".//DESCRIPTION_SHORT[@lang='eng']")[0] != [] else 'n/a'
+
+    long_description = product_details.findall(".//DESCRIPTION_LONG[@lang='eng']")[0].text if \
+        product_details.findall(".//DESCRIPTION_LONG[@lang='eng']") != [] else 'n/a'
+    manufacturer_name = product_details.find('MANUFACTURER_NAME').text if product_details.find(
+        'MANUFACTURER_NAME') is not None else 'n/a'
+    manufacturer_type_descr = product_details.find('MANUFACTURER_TYPE_DESCR').text if product_details.find(
+        'MANUFACTURER_TYPE_DESCR') is not None else 'n/a'
     meta_key_words_list = product_details.findall(".//KEYWORD[@lang='eng']")
-    meta_key_words = ', '.join([keyword.text for keyword in meta_key_words_list])
+    meta_key_words = ', '.join([keyword.text if keyword is not None else '' for keyword in meta_key_words_list])
 
-    order_unit = product_order_details.find('ORDER_UNIT').text
-    content_unit = product_order_details.find('CONTENT_UNIT').text
-    number_cu_per_ou = product_order_details.find('NO_CU_PER_OU').text
-    price_quantity = product_order_details.find('PRICE_QUANTITY').text
-    quantity_min = product_order_details.find('QUANTITY_MIN').text
-    quantity_interval = product_order_details.find('QUANTITY_INTERVAL').text
-    price_amount = product_price.find('PRICE_AMOUNT').text
-    price_currency = product_price.find('PRICE_CURRENCY').text
-    tax = product_price.find('TAX').text
+    order_unit = product_order_details.find('ORDER_UNIT').text if product_order_details.find(
+        'ORDER_UNIT') is not None else 'n/a'
+    content_unit = product_order_details.find('CONTENT_UNIT').text if product_order_details.find(
+        'CONTENT_UNIT') is not None else 'n/a'
+    number_cu_per_ou = product_order_details.find('NO_CU_PER_OU').text if product_order_details.find(
+        'NO_CU_PER_OU') is not None else 'n/a'
+    price_quantity = product_order_details.find('PRICE_QUANTITY').text if product_order_details.find(
+        'PRICE_QUANTITY') is not None else 'n/a'
+    quantity_min = product_order_details.find('QUANTITY_MIN').text if product_order_details.find(
+        'QUANTITY_MIN') is not None else 'n/a'
+    quantity_interval = product_order_details.find('QUANTITY_INTERVAL').text if product_order_details.find(
+        'QUANTITY_INTERVAL') is not None else 'n/a'
+    price_amount = product_price.find('PRICE_AMOUNT').text if product_price.find('PRICE_AMOUNT') is not None else 'n/a'
+    price_currency = product_price.find('PRICE_CURRENCY').text if product_price.find(
+        'PRICE_CURRENCY') is not None else 'n/a'
+    tax = product_price.find('TAX').text if product_price.find('TAX') is not None else 'n/a'
     photo_normal = remove_none([mime.find('MIME_SOURCE') if (mime.find('MIME_PURPOSE').text == 'normal') else None for
                                 mime in mime_info], True)
     photo_detail = remove_none([mime.find('MIME_SOURCE') if (mime.find('MIME_PURPOSE').text == 'detail') else None for
@@ -60,11 +78,16 @@ for each_product in root.findall('PRODUCT'):
     data_sheet = remove_none([mime.find('MIME_SOURCE') if (mime.find('MIME_PURPOSE').text == 'data_sheet') and (
             'locale=en_GB' in mime.find('MIME_SOURCE').text) else None for
                               mime in mime_info], True)
-    volume = packing_unit.find('UDX.EDXF.VOLUME').text
-    weight = packing_unit.find('UDX.EDXF.WEIGHT').text
-    length = packing_unit.find('UDX.EDXF.LENGTH').text
-    width = packing_unit.find('UDX.EDXF.WIDTH').text
-    depth = packing_unit.find('UDX.EDXF.DEPTH').text
+    volume = packing_unit.find('UDX.EDXF.VOLUME').text if (packing_unit is not None) and (
+            packing_unit.find('UDX.EDXF.VOLUME') is not None) else 'n/a'
+    weight = packing_unit.find('UDX.EDXF.WEIGHT').text if (packing_unit is not None) and (
+            packing_unit.find('UDX.EDXF.WEIGHT') is not None) else 'n/a'
+    length = packing_unit.find('UDX.EDXF.LENGTH').text if (packing_unit is not None) and (
+            packing_unit.find('UDX.EDXF.LENGTH') is not None) else 'n/a'
+    width = packing_unit.find('UDX.EDXF.WIDTH').text if (packing_unit is not None) and (
+            packing_unit.find('UDX.EDXF.WIDTH') is not None) else 'n/a'
+    depth = packing_unit.find('UDX.EDXF.DEPTH').text if (packing_unit is not None) and (
+            packing_unit.find('UDX.EDXF.DEPTH') is not None) else 'n/a'
     products.append(
         Product(product_id = product_id, international_pid = international_pid, manufacturer_pid = manufacturer_pid,
                 short_description = short_description, long_description = long_description,
